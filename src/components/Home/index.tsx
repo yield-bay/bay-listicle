29ff6d6ec32144fd3950b139eba12d0dadd118b9
Luke Skywalker <skywalker.eth@protonmail.com>
2022-07-01 18:27:46 +0530

    code structure and packages

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div>
+      <p>Hello there!</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Home from "../components/Home";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default Index;
