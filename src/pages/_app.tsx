c0b2aa6586a68229a909d779da673db0975f0a82
nightwing-yb <nightwing-of-the-bay@protonmail.com>
2022-07-10 04:34:37 +0530

    setup fathom analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,10 @@ import { ThemeProvider } from "next-themes";
 
 // Stylesheet Imports
 import "../styles/globals.css";
+import useFathomSetup from "@hooks/useFathomSetup";
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useFathomSetup(process.env.NEXT_PUBLIC_FATHOM_CODE as string);
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
