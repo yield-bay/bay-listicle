a9f03fec2ee2a972e628564ae7e39c137495cf0f
Luke Skywalker <skywalker.eth@protonmail.com>
2022-07-16 19:43:07 +0530

    🔃 sorting in farmlist

--- src/components/Home/ListicleTable.tsx
+++ src/components/Home/ListicleTable.tsx
@@ -1,8 +1,61 @@
-import { QuestionMarkCircleIcon } from "@heroicons/react/outline";
+import { useEffect, useState } from "react";
+import {
+  ArrowNarrowDownIcon,
+  ArrowNarrowUpIcon,
+  QuestionMarkCircleIcon,
+} from "@heroicons/react/outline";
 import FarmsList from "./FarmsList";
 import Tooltip from "@components/Tooltip";
 
+enum Order {
+  ASC,
+  DESC,
+}
+
 const ListicleTable = ({ farms }: any) => {
+  const [sortStatus, setSortStatus] = useState({
+    key: "tvl",
+    order: Order.DESC,
+  });
+
+  const [sortedFarms, setSortedFarms] = useState(farms);
+
+  useEffect(() => {
+    if (farms.length > 0) setSortedFarms(farms);
+  }, [farms]);
+
+  const handleSort = (key: string) => {
+    let newSortStatus = {
+      key,
+      order: sortStatus.order == Order.ASC ? Order.DESC : Order.ASC, // Flip the order
+    };
+    if (key !== newSortStatus.key) newSortStatus.order = Order.DESC; // if the key is not same as before, set the Order to DESC
+    setSortStatus(newSortStatus);
+
+    let sortFn; // to be used to sort the pools
+    if (newSortStatus.key == "tvl") {
+      sortFn = (a: any, b: any) =>
+        newSortStatus.order == Order.ASC
+          ? a.tvl >= b.tvl
+            ? 1
+            : -1
+          : a.tvl < b.tvl
+          ? 1
+          : -1;
+    } else if (newSortStatus.key == "yield") {
+      sortFn = (a: any, b: any) =>
+        newSortStatus.order == Order.ASC
+          ? a.apr.farm + a.apr.trading >= b.apr.farm + b.apr.trading
+            ? 1
+            : -1
+          : a.apr.farm + a.apr.trading < b.apr.farm + b.apr.trading
+          ? 1
+          : -1;
+    }
+
+    setSortedFarms([...farms].sort(sortFn));
+  };
+
   return (
     <div className="flex flex-col">
       <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -19,31 +72,43 @@ const ListicleTable = ({ farms }: any) => {
                   </th>
                   <th
                     scope="col"
-                    className="px-3 py-3.5 text-left text-sm font-medium tracking-wider"
+                    className="px-3 py-3.5 text-left text-sm font-medium tracking-wider cursor-pointer"
+                    onClick={() => handleSort("tvl")}
                   >
                     <div className="flex items-center">
-                      <span>TVL</span>
                       <Tooltip
-                        tooltipText={
-                          "Total Value Locked. Amount of money currently invested in the farm, denoted in USD."
-                        }
+                        tooltipText="Total Value Locked. Amount of money currently invested in the farm, denoted in USD."
+                        onButtonClick={() => handleSort("tvl")}
                       >
-                        <QuestionMarkCircleIcon className="w-4 h-5 ml-1 text-neutral-500 dark:text-neutral-400" />
+                        <span>TVL</span>
+                        {/* <QuestionMarkCircleIcon className="w-4 h-5 ml-1 text-neutral-500 dark:text-neutral-400" /> */}
+                        {sortStatus.key == "tvl" &&
+                          (sortStatus.order == Order.DESC ? (
+                            <ArrowNarrowDownIcon className="w-4 h-4 inline -mt-0.5" />
+                          ) : (
+                            <ArrowNarrowUpIcon className="w-4 h-4 inline mb-0.5" />
+                          ))}
                       </Tooltip>
                     </div>
                   </th>
                   <th
                     scope="col"
-                    className="flex px-3 py-3.5 text-left text-sm font-medium tracking-wider"
+                    className="flex px-3 py-3.5 text-left text-sm font-medium tracking-wider cursor-pointer"
+                    onClick={() => handleSort("yield")}
                   >
                     <div className="flex items-center">
-                      <span>Yield</span>
                       <Tooltip
-                        tooltipText={
-                          "The percentage of returns the farm offers on staking for an year."
-                        }
+                        tooltipText="The percentage of returns the farm offers on staking for an year."
+                        onButtonClick={() => handleSort("yield")}
                       >
-                        <QuestionMarkCircleIcon className="w-4 h-4 ml-1 text-neutral-500 dark:text-neutral-400" />
+                        <span>Yield</span>
+                        {/* <QuestionMarkCircleIcon className="w-4 h-4 ml-1 text-neutral-500 dark:text-neutral-400" /> */}
+                        {sortStatus.key == "yield" &&
+                          (sortStatus.order == Order.DESC ? (
+                            <ArrowNarrowDownIcon className="w-4 h-4 inline -mt-0.5" />
+                          ) : (
+                            <ArrowNarrowUpIcon className="w-4 h-4 inline mb-0.5" />
+                          ))}
                       </Tooltip>
                     </div>
                   </th>
@@ -53,7 +118,7 @@ const ListicleTable = ({ farms }: any) => {
                 </tr>
               </thead>
               <tbody className="divide-y divide-neutral-200 dark:divide-neutral-600 bg-white dark:bg-neutral-800">
-                <FarmsList farms={farms} />
+                <FarmsList farms={sortedFarms} />
               </tbody>
             </table>
           </div>
